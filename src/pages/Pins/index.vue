d901306879fde036bd7f8841ddc33aeb0d9cf2bf
vn <1017427859@qq.com>
2020-10-09 15:35:24 +0800

    feat:添加沸点组件

--- src/pages/Pins/index.vue
+++ src/pages/Pins/index.vue
@@ -1,6 +1,239 @@
 <template>
-  <div>
-    Pins...
+ <div class="container">
+    <!-- 左侧导航栏 -->
+    <div class="dock">
+      <nav class="dock-nav" role="navigation">
+        <ul class="nav-list">
+          <li class="nav-item one active">
+            <a href="javascript:;">推荐</a>
+          </li>
+          <li class="nav-item">
+            <a href="javascript:;">热门</a>
+          </li>
+          <li class="nav-item">
+            <a href="javascript:;">关注</a>
+          </li>
+          <li class="nav-item">
+            <a href="javascript:;">上班摸鱼</a>
+          </li>
+          <li class="nav-item">
+            <a href="javascript:;">内推招聘</a>
+          </li>
+          <li class="nav-item">
+            <a href="javascript:;">一图胜千言</a>
+          </li>
+          <li class="nav-item">
+            <a href="javascript:;">今天学到了</a>
+          </li>
+          <li class="nav-item">
+            <a href="javascript:;">每天一到...</a>
+          </li>
+          <li class="nav-item">
+            <a href="javascript:;">开发工具</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <!-- 内容区 -->
+    <div class="stream">
+      <div class="stream-wrapper">
+        <div class="pin-list-view">
+          <ul class="pin-list">
+            <li class="item shadow">
+              <div class="pin">
+                <!-- 头部 -->
+                <div class="pin-header-row">
+                  <div class="account-group">
+                    <!-- 图片 -->
+                    <div class="user-popover-box">
+                      <a href="javascript:;" class="user-link">
+                        <div class="lazy avatar avatar loaded"></div>
+                      </a>
+                    </div>
+                    <!-- 用户名字和信息 -->
+                    <div class="pin-header-content">
+                      <div class="user-popover">
+                        <a href="" class="username">mlgg</a>
+                      </div>
+                      <div class="meta-box">
+                        <div class="position ellipsis">前端</div>
+                        <div class="dot">.</div>
+                        <a href="javascript">9小时前</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="header-action">
+                    <button class="subscribe-btn follow-button">
+                      <span>关注</span>
+                    </button>
+                  </div>
+                </div>
+                <!-- 标间 -->
+                <div class="pin-content-row">
+                  <div class="content-box content-box">
+                    <span>养生牢记于心</span>
+                  </div>
+                </div>
+                <!-- 图片 -->
+                <div class="pin-image-row">
+                  <div class="image-box-wrapper image-box">
+                    <div class="image-box">
+                      <div class="image"></div>
+                    </div>
+
+                  </div>
+                </div>
+                <!-- 添加的标签 -->
+                <div class="pin-topic-row">
+                  <a href="javascript" class="topic-title">一图胜千言</a>
+                </div>
+                <!-- 标签底部 -->
+                <div class="pin-action-row">
+                  <div class="action-box action-box">
+                    <div class="like-action action">
+                      <span class="iconfont icon-dianzan"></span>
+                      <span>2</span>
+                    </div>
+                    <div class="like-action action">
+                      <span class="iconfont icon-pinglun_huabanfuben"></span>
+                      <span>评论</span>
+                    </div>
+                    <div class="like-action action">
+                      <span class="iconfont icon-shangchuan"></span>
+                      <span>分享</span>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="item shadow">
+              <div class="pin">
+                <!-- 头部 -->
+                <div class="pin-header-row">
+                  <div class="account-group">
+                    <!-- 图片 -->
+                    <div class="user-popover-box">
+                      <a href="javascript:;" class="user-link">
+                        <div class="lazy avatar avatar loaded"></div>
+                      </a>
+                    </div>
+                    <!-- 用户名字和信息 -->
+                    <div class="pin-header-content">
+                      <div class="user-popover">
+                        <a href="" class="username">mlgg</a>
+                      </div>
+                      <div class="meta-box">
+                        <div class="position ellipsis">前端</div>
+                        <div class="dot">.</div>
+                        <a href="javascript">9小时前</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="header-action">
+                    <button class="subscribe-btn follow-button">
+                      <span>关注</span>
+                    </button>
+                  </div>
+                </div>
+                <!-- 标间 -->
+                <div class="pin-content-row">
+                  <div class="content-box content-box">
+                    <span>养生牢记于心</span>
+                  </div>
+                </div>
+                <!-- 图片 -->
+                <div class="pin-image-row">
+                  <div class="image-box-wrapper image-box">
+                    <div class="image-box">
+                      <div class="image"></div>
+                    </div>
+
+                  </div>
+                </div>
+                <!-- 添加的标签 -->
+                <div class="pin-topic-row">
+                  <a href="javascript" class="topic-title">一图胜千言</a>
+                </div>
+                <!-- 标签底部 -->
+                <div class="pin-action-row">
+                  <div class="action-box action-box">
+                    <div class="like-action action">
+                      <span class="iconfont icon-dianzan"></span>
+                      <span>2</span>
+                    </div>
+                    <div class="like-action action">
+                      <span class="iconfont icon-pinglun_huabanfuben"></span>
+                      <span>评论</span>
+                    </div>
+                    <div class="like-action action">
+                      <span class="iconfont icon-shangchuan"></span>
+                      <span>分享</span>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!-- 右侧导航 -->
+    <div class="pin__side sidebar">
+      <div class="related-pin-block pin-block">
+        <header class="">推荐浮点</header>
+        <ul class="pin-list">
+          <li class="item">
+            <a href="javascript:;" class="pin">
+              <div class="content-box">
+                <div class="content with-picture">
+                  对不起大家，我是废物，在家打了5天游戏😭
+                </div>
+                <div class="stat">
+                  <span>3 赞 .</span>
+                  <span>22 评论</span>
+                </div>
+              </div>
+              <div class="image-box"></div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="javascript:;" class="pin">
+              <div class="content-box">
+                <div class="content with-picture">
+                  对不起大家，我是废物，在家打了5天游戏😭
+                </div>
+                <div class="stat">
+                  <span>3 赞 .</span>
+                  <span>22 评论</span>
+                </div>
+              </div>
+              <div class="image-box"></div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="javascript:;" class="pin">
+              <div class="content-box">
+                <div class="content with-picture">
+                  对不起大家，我是废物，在家打了5天游戏😭
+                </div>
+                <div class="stat">
+                  <span>3 赞 .</span>
+                  <span>22 评论</span>
+                </div>
+              </div>
+              <div class="image-box"></div>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <a href="javascript" class="guide-link shadow">
+        <img src="./img/svg_xml_base64_PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzNiIgaGVpZ2h0PSIzNiIgd.svg" alt="" class="icon">
+        <span class="title">如何玩转沸点</span>
+      </a>
+    </div>
   </div>
 </template>
 
@@ -11,5 +244,242 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.container {
+  display: flex;
+  width: 960px;
+  height: 1800px;
+  margin: 0 auto;
 
+  background-color: #ffffff;
+  margin-top: 20px;
+  margin-bottom: 72px;
+  position: relative;
+  .dock {
+    width: 112px;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+    .dock-nav {
+      background-color: skyblue;
+      border-radius: 2px;
+      //background-color: yellow;
+      position: fixed;
+      left: 280px;
+      top: 80px;
+      .nav-list {
+        display: flex;
+        flex-direction: column;
+        padding: 16px 12px;
+        .nav-item {
+          padding: 0 6px;
+          text-align: center;
+          max-width: 100%;
+          height: 31px;
+          line-height: 31px;
+          color: #909090;
+          margin-top: 10px;
+          &.one {
+            margin-top: 0px;
+          }
+        }
+        .active {
+          color: white;
+          background-color: #007fff;
+          border-radius: 2px;
+          a {
+            color: white;
+          }
+        }
+      }
+    }
+  }
+  .stream {
+    width: 570px;
+    height: 800px;
+    // background-color: skyblue;
+    margin-right: 15px;
+    .stream-wrapper {
+      .pin-list-view {
+        .pin-list {
+          .item {
+            margin-bottom: 8px;
+            border: 1px solid #ebebeb;
+            .pin {
+              .pin-header-row {
+                display: flex;
+                padding: 16px 24px 0 20px;
+                justify-content: space-between;
+                align-items: center;
+                // background-color: yellow;
+                .account-group {
+                  display: flex;
+                  justify-items: center;
+                  .user-popover-box {
+                    width: 45px;
+                    height: 48px;
+
+                    .user-link {
+                      .lazy {
+                        width: 45px;
+                        height: 45px;
+                        border-radius: 50%;
+                        background-image: url('../Folat/img/1.jpg');
+                        background-size: 45px;
+                      }
+                    }
+                  }
+                  .pin-header-content {
+                    margin-left: 12px;
+                    .user-popover {
+                      margin-top: 4px;
+                      .username {
+                        color: #2e3135;
+                        font-weight: 600;
+                      }
+                    }
+                    .meta-box {
+                      margin-top: 4px;
+                      .position {
+                        display: inline;
+                      }
+                      .dot {
+                        display: inline;
+                      }
+                    }
+                  }
+                }
+                .header-action {
+                  .subscribe-btn {
+                    outline: none;
+                    border: 0 none;
+                    width: 55px;
+                    height: 26px;
+                    font-size: 13px;
+                    border-color: #6cbd45;
+                    color: #6cbd45;
+                    padding: 0;
+                    margin: 0 0 0 auto;
+                    cursor: pointer;
+                  }
+                }
+              }
+              .pin-content-row {
+                margin: 5px 48px 5px 77px;
+              }
+              .pin-image-row {
+                margin: 4px 48px 0 77px;
+                .image-box-wrapper {
+                  .image-box {
+                    .image {
+                      margin-top: 4px;
+                      background: url('../Folat/img/f433295fd091456ba9e55bde14cadb56_tplv-k3u1fbpfcp-zoom-1.jpg');
+                      background-repeat: no-repeat;
+                      width: 200px;
+                      height: 140px;
+                      background-size: 200px;
+                    }
+                  }
+                }
+              }
+              .pin-topic-row {
+                margin: 7px 48px 0 77px;
+                .topic-title {
+                  display: inline-block;
+                  font-size: 13px;
+                  padding: 0 12px;
+                  border: 1px solid #007fff;
+                  border-radius: 14px;
+                  color: #007fff;
+                }
+              }
+              .pin-action-row {
+                .action-box {
+                  display: flex;
+                  margin-top: 16px;
+                  border-top: 1px solid #ebebeb;
+                  height: 34px;
+                  text-align: center;
+                  .like-action {
+                    width: 190px;
+                    height: 34px;
+                    line-height: 34px;
+                    &::after {
+                      display: block;
+                      position: relative;
+                      top: -30px;
+                      content: '';
+                      width: 1px;
+                      height: 24px;
+                      background-color: #ebebeb;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .pin__side {
+    width: 263px;
+    height: 400px;
+    position: fixed;
+    right: 278px;
+    //background-color: pink;
+    .related-pin-block {
+      border: 1px solid hsla(0, 0%, 59.2%, 0.1);
+      margin-bottom: 8px;
+      header {
+        color: #2e3135;
+        font-size: 13px;
+        font-weight: 600;
+        padding: 13px 16px;
+        border-bottom: 1px solid hsla(0, 0%, 59.2%, 0.1);
+      }
+      .pin-list {
+        padding: 0 16px;
+        .item {
+          padding: 12px 0;
+          border-bottom: 1px solid hsla(0, 0%, 59.2%, 0.1);
+          .pin {
+            display: flex;
+            text-decoration: none;
+            justify-content: space-between;
+            .content-box {
+              width: 100px;
+              .content {
+                display: -webkit-box;
+                color: #2e3135;
+                -webkit-box-orient: vertical;
+                text-overflow: ellipsis;
+                overflow: hidden;
+                -webkit-line-clamp: 2;
+              }
+              .stat {
+                margin-top: 10px;
+              }
+            }
+            .image-box {
+              width: 68px;
+              height: 68px;
+              //background-color: skyblue;
+              background-image: url('../Folat/img/7b54f66b14ea45eeb84846dc34e11598_tplv-k3u1fbpfcp-zoom-1.png');
+              background-size: cover;
+              background-repeat: no-repeat;
+            }
+          }
+        }
+      }
+    }
+    .guide-link {
+      display: flex;
+      padding: 15px;
+      align-items: center;
+      margin-bottom: 8px;
+      border: 1px solid hsla(0, 0%, 59.2%, 0.1);
+      .icon {
+        margin-right: 10px;
+      }
+    }
+  }
+}
 </style>
