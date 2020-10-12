4eb8fed9e6dbdc83f580f0b06d22396a16a4e446
huanli <l815791478@163.com>
2020-10-12 14:22:44 +0800

    feat:话题页面动态数据展示

--- src/api/topics/index.js
+++ src/api/topics/index.js
@@ -2,11 +2,11 @@ import request from "@/utils/request";
 
 export default {
   // 获取话题列表
-  // getTopicList(topicListParams) {
-  //   return request({
-  //     url: "/tag_api/v1/query_topic_list",
-  //     method: "POST",
-  //     data: topicListParams,
-  //   });
-  // },
+  getTopicList(topicListParams) {
+    return request({
+      url: "/zhuanfa/tag_api/v1/query_topic_list",
+      method: "POST",
+      data: topicListParams,
+    });
+  },
 };
